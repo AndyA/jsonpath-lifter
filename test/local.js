57d2f4b034e0293ea7abfcdf9e8edccc41360322
Andy Armstrong <andy@hexten.net>
2020-09-10 19:04:36 +0100

    Local storage.

--- test/local.js
+++ test/local.js
@@ -5,12 +5,26 @@ const _ = require("lodash");
 
 const lifter = require("..");
 
-tap.test("access local storage", async () => {
+if (0)
+  tap.test("access local storage", async () => {
+    const lift = lifter(
+      { set: "OK", dst: "$.status" },
+      { src: "@.id", dst: "$.ident" }
+    );
+    const got = lift({}, {}, { local: { id: "ABC" } });
+    const want = { status: "OK", ident: "ABC" };
+    tap.same(got, want, "fetch from local");
+  });
+
+tap.test("use local storage", async () => {
   const lift = lifter(
-    { set: "OK", dst: "$.status" },
-    { src: "@.id", dst: "$.ident" }
+    { dst: "@.id", src: "$.ident" },
+    { dst: "$.status", set: "OK" },
+    { dst: "$.ID", src: "@.id" }
   );
-  const got = lift({}, {}, { local: { id: "ABC" } });
-  const want = { status: "OK", ident: "ABC" };
-  tap.same(got, want, "fetch from local");
+
+  const doc = { ident: "XYZ" };
+  const got = lift(doc);
+  const want = { ID: "XYZ", status: "OK" };
+  tap.same(got, want, "use local");
 });
