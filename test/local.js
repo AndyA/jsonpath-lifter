7e3c7f9c9e5d0f69c8a81585ce41f296f6cb945c
Andy Armstrong <andy@hexten.net>
2020-09-11 10:32:40 +0100

    Fix vivification via local value.

--- test/local.js
+++ test/local.js
@@ -5,6 +5,13 @@ const _ = require("lodash");
 
 const lifter = require("..");
 
+const lower = s => s.toLowerCase();
+const reverse = s =>
+  s
+    .split("")
+    .reverse()
+    .join("");
+
 tap.test("access local storage", async () => {
   const lift = lifter(
     { set: "OK", dst: "$.status" },
@@ -17,13 +24,39 @@ tap.test("access local storage", async () => {
 
 tap.test("use local storage", async () => {
   const lift = lifter(
-    { dst: "@.id", src: "$.ident" },
+    { dst: "@.id", src: "$.ident", via: lower },
     { dst: "$.status", set: "OK" },
-    { dst: "$.ID", src: "@.id" }
+    { dst: "$.ID", src: "@.id", via: reverse }
   );
 
   const doc = { ident: "XYZ" };
   const got = lift(doc);
-  const want = { ID: "XYZ", status: "OK" };
+  const want = { ID: "zyx", status: "OK" };
   tap.same(got, want, "use local");
+});
+
+tap.test("local storage scope", async () => {
+  const innerLift = lifter(
+    { dst: "@.id", src: "$.ident", via: lower },
+    { dst: "$.ID", src: "@.id", via: reverse }
+  );
+
+  const lift = lifter(
+    { dst: "@.id", src: "$.ident", via: lower },
+    { src: "$.parts[*]", via: innerLift },
+    { dst: "$.ID", src: "@.id", via: reverse }
+  );
+
+  const doc = {
+    ident: "XYZ",
+    parts: [{ ident: "ABC" }, { ident: "DEF" }, { ident: "GHI" }]
+  };
+
+  const want = {
+    ID: "zyx",
+    parts: [{ ID: "cba" }, { ID: "fed" }, { ID: "ihg" }]
+  };
+
+  const got = lift(doc);
+  tap.same(got, want, "nested local storage");
 });
--- tools/mule.js
+++ tools/mule.js
@@ -2,7 +2,31 @@
 
 const lifter = require("..");
 
-const doc = { id: "ABC" };
-const lift = lifter({ src: "$" });
+const lower = s => s.toLowerCase();
+const reverse = s =>
+  s
+    .split("")
+    .reverse()
+    .join("");
+
+const innerLift = lifter(
+  { dst: "@.id", src: "$.ident", via: lower },
+  { dst: "$.ID1", src: "@.id" },
+  { dst: "$.ID", src: "$.ident", via: lower },
+  { dst: "$.ID2", src: "@.id", via: reverse }
+);
+
+const lift = lifter(
+  //  { dst: "@.id", src: "$.ident", via: lower },
+  { src: "$.parts[*]", via: innerLift }
+  //  , { dst: "$.ID", src: "@.id", via: reverse }
+);
+
+const doc = {
+  ident: "XYZ",
+  parts: [{ ident: "ABC" } /*, { ident: "DEF" }, { ident: "GHI" }*/]
+};
+
 const got = lift(doc);
-console.log(got);
+
+console.log("result", got);
